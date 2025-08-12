19bbdf684f3d431b93412aa89c2a66acbb7970de
Pavel642-bit <centrum7171@gmail.com>
2025-08-12 01:10:40 -0700

    Update BigOnotations.js

--- BigOnotations.js
+++ BigOnotations.js
@@ -2,8 +2,9 @@
 
 let n = 5;
 
-if (n == 1) return;
-
+if (n == 1) {
+    return;
+}
 for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
         console.log("*");
@@ -16,16 +17,16 @@ for (let i = 1; i <= n; i++) {
 
 // task #2
 
-// let n = 10;
-// let i = 0;
-// let j = 0;
-// let a = 0;
+let n = 10;
+let i = 0;
+let j = 0;
+let a = 0;
 
-// for (i = (n / 2); i <= n; i = i + 1) {
-//     for (j = 2; j <= n; j = j * 2) {
-//         a = a + n / 2;
-//     }
-// }
+for (i = (n / 2); i <= n; i = i + 1) {
+    for (j = 2; j <= n; j = j * 2) {
+        a = a + n / 2;
+    }
+}
 
 // O(n log n).
 
@@ -33,15 +34,15 @@ for (let i = 1; i <= n; i++) {
 
 // task #3
 
-// let n = 5;
-// let a = 0;
+let n = 5;
+let a = 0;
 
-// for (let i = 0; i < n; i = i + 1) {
-//     for (let j = n; j > i; j = j - 1) {
-//         a = a + i + j;
-//     }
-// }
-// return a;
+for (let i = 0; i < n; i ++) {
+    for (let j = n; j > i; j --) {
+        a = a + i + j;
+    }
+}
+return a;
 
 
 // O(n ^ 2).
@@ -50,14 +51,14 @@ for (let i = 1; i <= n; i++) {
 
 // task #4
 
-// let n = 10;
-// let a = 0;
-// let i = n;
+let n = 10;
+let a = 0;
+let i = n;
 
-// while (i > 0) {
-//     a = a + i;
-//     i = (i / 2);
-// }
+while (i > 0) {
+    a = a + i;
+    i = (i / 2);
+}
 // O(log n)
 
 
