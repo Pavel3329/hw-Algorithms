3ec8f8340aac3d78387604f2655cd2ce182c633a
Pavel3329 <214103429+Pavel3329@users.noreply.github.com>
2025-09-01 23:34:18 +0200

    fibonacci-DaC.js

--- fibonacci-DaC.js
+++ fibonacci-DaC.js
@@ -4,30 +4,33 @@
 // рекурсивная реализация (наивная)
 
 
-// function fib(n) {
-//   if (n === 1 || n === 2) return 1;
-//   return fib(n - 1) + fib(n - 2);
-// }
+function fib(n) {
+  if (n === 1 || n === 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
 
-// for (let i = 1; i <= 11; i++) {
-//   console.log(fib(i));
-// }
+for (let i = 1; i <= 11; i++) {
+  console.log(fib(i));
+}
 
 // // .............
 // // Улучшение через мемоизацию (алгоритм РиВ = "разделяй и властвуй")
 
-// function fib(n, memo = {}) {
-//   if (n in memo) return memo[n];     // уже считали → возвращаем
-//   if (n === 1 || n === 2) return 1;
+function fib(n, memo = {}) {
+  if (n in memo) return memo[n];     // уже считали → возвращаем
+  if (n === 1 || n === 2) return 1;
 
-//   memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
-//   return memo[n];
-// }
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+}
 
-// for (let i = 1; i <= 11; i++) {
-//   console.log(fib(i));
-// }
+for (let i = 1; i <= 11; i++) {
+  console.log(fib(i));
+}
 // .................
+
+
+// два отсортированных массива, - найти элемент: к = 7
 
 
 const arr1 = [100, 112, 256, 349, 770];
